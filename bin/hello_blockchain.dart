9d36842885948654f633c878e5bbe7d686eae550
AlanHee <alan.hee@outlook.com>
2025-02-11 06:37:44 +0800

    Tue

---
+++ bin/hello_blockchain.dart
@@ -0,0 +1,5 @@
+import 'package:hello_blockchain/hello_blockchain.dart' as hello_blockchain;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hello_blockchain.calculate()}!');
+}
--- lession2/gen_public_key.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-openssl genrsa -out private_key.pem 2048
